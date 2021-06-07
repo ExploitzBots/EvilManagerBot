910a7071d2e0c6eff91e5436472ca7a2069eb1b1
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-06-07 12:22:13 +0530

    Update greetings.py

--- EMBot/plugins/greetings.py
+++ EMBot/plugins/greetings.py
@@ -1,10 +1,10 @@
-from EMBot import evil
+from EMBot import Evil
 from telethon import events, Button
 from telethon.tl.functions.users import GetFullUserRequest
 from datetime import timedelta
 from Configs import Config
 
-@evil.on(events.CallbackQuery(pattern=r"check-bot-(\d+)"))
+@Evil.on(events.CallbackQuery(pattern=r"check-bot-(\d+)"))
 async def check(event):
 
     user_id = int(event.pattern_match.group(1))
@@ -13,18 +13,18 @@ async def check(event):
         await event.answer("You can already speak freely!", alert=True)
         return
     if event.sender_id == user_id:
-            await evil.edit_permissions(chat_id, event.sender_id, send_messages=True)
+            await Evil.edit_permissions(chat_id, event.sender_id, send_messages=True)
             await event.answer("You are succesfully unmuted!")
             await event.edit(Config.WELCOME_TEXT, buttons=[
             [Button.url("Chat Rules!", "t.me/{}?start=rules".format(Config.BOT_US))]
             ], parse_mode="HTML", link_preview=False)
 
 
-@evil.on(events.ChatAction)
+@Evil.on(events.ChatAction)
 async def join(event):
 
     if event.user_joined:
-        await evil.edit_permissions(event.chat_id, event.user_id, send_messages=False)
+        await Evil.edit_permissions(event.chat_id, event.user_id, send_messages=False)
         await event.reply(Config.WELCOME_TEXT, parse_mode="HTML", link_preview=False, buttons=[
         [Button.inline("Click Here to unmute yourslef!", data=f"check-bot-{event.user_id}")]
         ])
