c1be400185e47cfdd30ee34478abd8d84932eccf
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-06-07 12:15:42 +0530

    Update utils.py

--- EMBot/utils.py
+++ EMBot/utils.py
@@ -1,1 +1,15 @@
+import sys
+import logging
+import importlib
+from pathlib import Path
+
+def load_plugins(plugin_name):
+    path = Path(f"EMBot/plugins/{plugin_name}.py")
+    name = "EMBot.plugins.{}".format(plugin_name)
+    spec = importlib.util.spec_from_file_location(name, path)
+    load = importlib.util.module_from_spec(spec)
+    load.logger = logging.getLogger(plugin_name)
+    spec.loader.exec_module(load)
+    sys.modules["EMBot.plugins." + plugin_name] = load
+    print("Bot has Started " + plugin_name)
 
