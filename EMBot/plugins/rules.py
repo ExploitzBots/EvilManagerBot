84b2154d1293162c785ab474b65ace85cfda2bde
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-06-07 12:20:32 +0530

    Add files via upload

---
+++ EMBot/plugins/rules.py
@@ -0,0 +1,44 @@
+from EMBot import evil
+from telethon import events, Button
+from Configs import Config
+
+RULES = Config.RULES
+
+@evil.on(events.NewMessage(pattern="^[!?/]rules"))
+async def rules(event):
+
+    msg = await event.get_reply_message()
+    if not msg and event.is_group:
+        await event.delete()
+        await event.respond("Please read the rules before chatting here!", buttons=[
+        [Button.url("Chat Rules", "t.me/{}?start=rules".format(Config.BOT_US))
+]])
+        return
+        
+    re = (await event.get_reply_message()).id
+    await event.delete()
+    await evil.send_message(event.chat_id, "Please read the rules before chatting here!", buttons=[
+    [Button.url("Chat Rules", "t.me/{}?start=rules".format(Config.BOT_US))
+    ]], reply_to=re)
+    return
+
+    await event.reply(RULES)
+
+@evil.on(events.NewMessage(pattern="^/start rules"))
+async def rules(event):
+
+    if event.is_group:
+       await event.reply("Please read the rules before chatting here!", buttons=[
+       [Button.url("Chat Rules", "t.me/{}?start=rules".format(Config.BOT_US))
+       ]])
+       return
+
+    await event.reply(RULES)
+
+@evil.on(events.callbackquery.CallbackQuery(data="rules"))
+async def _(event):
+
+    await event.edit(RULES, buttons=[
+    [Button.inline("« Bᴀᴄᴋ", data="start")]
+    ])
+
