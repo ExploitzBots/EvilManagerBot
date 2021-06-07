061071a2d2687f0c4c8108e7ae55d11e8aa88f21
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-06-07 12:23:42 +0530

    Update locks.py

--- EMBot/plugins/locks.py
+++ EMBot/plugins/locks.py
@@ -1,5 +1,5 @@
 from telethon import events, Button, types
-from EMBot import evil
+from EMBot import Evil
 from EMBot.status import *
 
 LOCKS_HELP = """
@@ -10,7 +10,7 @@ LOCKS_HELP = """
 ‣ `?locktypes` - To get a list of modules can be locked
 """
 
-@evil.on(events.NewMessage(pattern="^[!?/]lock ?(.*)"))
+@Evil.on(events.NewMessage(pattern="^[!?/]lock ?(.*)"))
 @is_admin
 async def lock(event, perm):
     if not perm.change_info:
@@ -21,34 +21,34 @@ async def lock(event, perm):
        await event.reply("You haven't specified anything to lock.")
        return
     if "text" in input_str:
-       await evil.edit_permissions(event.chat_id, send_messages=False)
+       await Evil.edit_permissions(event.chat_id, send_messages=False)
        await event.reply("Locked `text`.")
     elif "media" in input_str:
-       await evil.edit_permissions(event.chat_id, send_media=False)
+       await Evil.edit_permissions(event.chat_id, send_media=False)
        await event.reply("Locked `media`.")
     elif "sticker" in input_str:
-       await evil.edit_permissions(event.chat_id, send_stickers=False)
+       await Evil.edit_permissions(event.chat_id, send_stickers=False)
        await event.reply("Locked `sticker`.")
     elif "gifs" in input_str:
-       await evil.edit_permissions(event.chat_id, send_gifs=False)
+       await Evil.edit_permissions(event.chat_id, send_gifs=False)
        await event.reply("Locked `gifs`.")
     elif "forward" in input_str:
-       await evil.edit_permissions(event.chat_id, forwards=False)
+       await Evil.edit_permissions(event.chat_id, forwards=False)
        await event.reply("Locked `forward`.")
     elif "games" in input_str:
-       await evil.edit_permissions(event.chat_id, send_games=False)
+       await Evil.edit_permissions(event.chat_id, send_games=False)
        await event.reply("Locked `games`.")
     elif "inline" in input_str:
-       await evil.edit_permissions(event.chat_id, send_inline=False)
+       await Evil.edit_permissions(event.chat_id, send_inline=False)
        await event.reply("Locked `inline`.")
     elif "polls" in input_str:
-       await evil.edit_permissions(event.chat_id, send_polls=False)
+       await Evil.edit_permissions(event.chat_id, send_polls=False)
        await event.reply("Locked `polls`.")
     elif "preview" in input_str:
-       await evil.edit_permissions(event.chat_id, embed_link_previews=False)
+       await Evil.edit_permissions(event.chat_id, embed_link_previews=False)
        await event.reply("Locked `preview`.")
     elif "all" in input_str:
-       await evil.edit_permissions(event.chat_id,
+       await Evil.edit_permissions(event.chat_id,
           send_messages=False, 
           send_media=False,
           send_stickers=False,
@@ -60,7 +60,7 @@ async def lock(event, perm):
        await event.reply("Locked `all`.")
 
 
-@evil.on(events.NewMessage(pattern="^[!?/]unlock ?(.*)"))
+@Evil.on(events.NewMessage(pattern="^[!?/]unlock ?(.*)"))
 @is_admin
 async def unlock(event, perm):
     if not perm.change_info:
@@ -71,34 +71,34 @@ async def unlock(event, perm):
        await event.reply("You haven't specified anything to unlock.")
        return
     if "text" in input_str:
-       await evil.edit_permissions(event.chat_id, send_messages=True)
+       await Evil.edit_permissions(event.chat_id, send_messages=True)
        await event.reply("Unlocked `text`.")
     elif "media" in input_str:
-       await evil.edit_permissions(event.chat_id, send_media=True)
+       await Evil.edit_permissions(event.chat_id, send_media=True)
        await event.reply("Unlocked `media`.")
     elif "sticker" in input_str:
-       await evil.edit_permissions(event.chat_id, send_stickers=True)
+       await Evil.edit_permissions(event.chat_id, send_stickers=True)
        await event.reply("Unlocked `sticker`.")
     elif "gifs" in input_str:
-       await evil.edit_permissions(event.chat_id, send_gifs=True)
+       await Evil.edit_permissions(event.chat_id, send_gifs=True)
        await event.reply("Unlocked `gifs`.")
     elif "forward" in input_str:
-       await evil.edit_permissions(event.chat_id, forwards=True)
+       await Evil.edit_permissions(event.chat_id, forwards=True)
        await event.reply("Unlocked `forward`.")
     elif "games" in input_str:
-       await evil.edit_permissions(event.chat_id, send_games=True)
+       await Evil.edit_permissions(event.chat_id, send_games=True)
        await event.reply("Unlocked `games`.")
     elif "inline" in input_str:
-       await evil.edit_permissions(event.chat_id, send_inline=True)
+       await Evil.edit_permissions(event.chat_id, send_inline=True)
        await event.reply("Unlocked `inline`.")
     elif "polls" in input_str:
-       await evil.edit_permissions(event.chat_id, send_polls=True)
+       await Evil.edit_permissions(event.chat_id, send_polls=True)
        await event.reply("Unlocked `polls`.")
     elif "preview" in input_str:
-       await evil.edit_permissions(event.chat_id, embed_link_previews=True)
+       await Evil.edit_permissions(event.chat_id, embed_link_previews=True)
        await event.reply("Unlocked `preview`.")
     elif "all" in input_str:
-       await evil.edit_permissions(event.chat_id,
+       await Evil.edit_permissions(event.chat_id,
           send_messages=True, 
           send_media=True,
           send_stickers=True,
@@ -110,7 +110,7 @@ async def unlock(event, perm):
        await event.reply("Unlocked `all`.")
 
 
-@evil.on(events.NewMessage(pattern="^[!?/]locktypes"))
+@Evil.on(events.NewMessage(pattern="^[!?/]locktypes"))
 async def locktypes(event):
     TEXT = """
 **Locks:**
@@ -127,7 +127,7 @@ async def locktypes(event):
 """
     await event.reply(TEXT)
 
-@evil.on(events.callbackquery.CallbackQuery(data="locks"))
+@Evil.on(events.callbackquery.CallbackQuery(data="locks"))
 async def _(event):
 
     await event.edit(LOCKS_HELP, buttons=[[Button.inline("« Bᴀᴄᴋ", data="help")]])
